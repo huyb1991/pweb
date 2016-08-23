bb6c987db7ee28b1b80b1de17394c6f7758b19d0
Huy Nguyen <huyb.1991@gmail.com>
2016-08-23 16:19:51 +0700

    Split ProfileCtrl

--- public/src/js/controllers.js
+++ public/src/js/controllers.js
@@ -1,14 +1,5 @@
 var personalControllers = angular.module('personalControllers', []);
-personalControllers.controller('ProfileCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('service/profile.json').then(function(response) {
-        var object = response.data;
-        $scope.desc = object.desc;
-        $scope.avatar = object.avatar;
-    });
-    $http.get('service/social.json').then(function(response) {
-        $scope.socials = response.data;
-    });
-}]);
+personalControllers.controller('ProfileCtrl', ['$scope', '$http', ProfileCtrl]);
 personalControllers.controller('ProjectsCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('service/project.json').then(function(response) {
         $scope.projects = response.data;
@@ -30,15 +21,13 @@ personalControllers.controller('NavCtrl', ['$scope', '$location', function($scop
     };
 }]);
 
-/*personalControllers.controller('ProfileCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('https://api.parse.com/1/classes/Profile', {
-        headers: {
-            'X-Parse-Application-Id': 'nUb4Im8Xvh9WJoJ7udFvdQ6qeg2g7DYYhR9mr6Ec',
-            'X-Parse-REST-API-Key': 'Vrcub7y0TKoA5aYW8i2tG1mogN8Rq4WyxpxBxv63'
-        }
-    }).then(function(response) {
-        var object = response.data.results[0];
-        $scope.desc = object.desc;
-        $scope.avatar = object.avatar;
-    });
-}]);*/
+function ProfileCtrl($scope, $http) {
+  $http.get('service/profile.json').then(function(response) {
+    var object = response.data;
+    $scope.desc = object.desc;
+    $scope.avatar = object.avatar;
+  });
+  $http.get('service/social.json').then(function(response) {
+    $scope.socials = response.data;
+  });
+}
