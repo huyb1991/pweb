dd4c9c04a6685a5276fd8c25758939cbe106cf28
Huy Nguyen <huyb.1991@gmail.com>
2016-08-23 17:20:58 +0700

    Applied IIFE

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,31 +1,34 @@
-angular
-  .module('personalApp', [
-    'ngRoute',
-    'ngAnimate',
-    'personalControllers'
-  ])
-  .config(['$routeProvider', '$locationProvider', appConfig]);
+(function() {
+  'use strict';
+  angular
+    .module('personalApp', [
+      'ngRoute',
+      'ngAnimate',
+      'personalControllers'
+    ])
+    .config(['$routeProvider', '$locationProvider', appConfig]);
 
-function appConfig($routeProvider,$locationProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl: 'profile.html',
-    controller: 'ProfileCtrl'
-  })
-  .when('/projects', {
-    templateUrl: 'projects.html',
-    controller: 'ProjectsCtrl'
-  })
-  .when('/books', {
-    templateUrl: 'books.html',
-    controller: 'BooksCtrl'
-  })
-  .when('/bookmarks', {
-    templateUrl: 'bookmarks.html',
-    controller: 'BookMarksCtrl'
-  })
-  .otherwise({
-    redirectTo:'/'
-  });
-  $locationProvider.html5Mode(true);
-}
+  function appConfig($routeProvider,$locationProvider) {
+    $routeProvider
+    .when('/', {
+      templateUrl: 'profile.html',
+      controller: 'ProfileCtrl'
+    })
+    .when('/projects', {
+      templateUrl: 'projects.html',
+      controller: 'ProjectsCtrl'
+    })
+    .when('/books', {
+      templateUrl: 'books.html',
+      controller: 'BooksCtrl'
+    })
+    .when('/bookmarks', {
+      templateUrl: 'bookmarks.html',
+      controller: 'BookMarksCtrl'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+    $locationProvider.html5Mode(true);
+  }
+})();
--- public/src/js/controllers.js
+++ public/src/js/controllers.js
@@ -1,41 +1,44 @@
-var personalControllers = angular.module('personalControllers', []);
-personalControllers.controller('ProfileCtrl', ['$scope', '$http', ProfileCtrl]);
-personalControllers.controller('ProjectsCtrl', ['$scope', '$http', ProjectsCtrl]);
-personalControllers.controller('BooksCtrl', ['$scope', '$http', BooksCtrl]);
-personalControllers.controller('BookMarksCtrl', ['$scope', '$http', BookMarksCtrl]);
-personalControllers.controller('NavCtrl', ['$scope', '$location', NavCtrl]);
+(function() {
+  'use strict';
+  angular.module('personalControllers', [])
+  .controller('ProfileCtrl', ['$scope', '$http', ProfileCtrl])
+  .controller('ProjectsCtrl', ['$scope', '$http', ProjectsCtrl])
+  .controller('BooksCtrl', ['$scope', '$http', BooksCtrl])
+  .controller('BookMarksCtrl', ['$scope', '$http', BookMarksCtrl])
+  .controller('NavCtrl', ['$scope', '$location', NavCtrl]);
 
-function ProfileCtrl($scope, $http) {
-  $http.get('service/profile.json').then(function(response) {
-    var object = response.data;
-    $scope.desc = object.desc;
-    $scope.avatar = object.avatar;
-  });
-  $http.get('service/social.json').then(function(response) {
-    $scope.socials = response.data;
-  });
-}
+  function ProfileCtrl($scope, $http) {
+    $http.get('service/profile.json').then(function(response) {
+      var object = response.data;
+      $scope.desc = object.desc;
+      $scope.avatar = object.avatar;
+    });
+    $http.get('service/social.json').then(function(response) {
+      $scope.socials = response.data;
+    });
+  }
 
-function ProjectsCtrl($scope, $http) {
-  $http.get('service/project.json').then(function(response) {
-    $scope.projects = response.data;
-  });
-}
+  function ProjectsCtrl($scope, $http) {
+    $http.get('service/project.json').then(function(response) {
+      $scope.projects = response.data;
+    });
+  }
 
-function BooksCtrl($scope, $http) {
-  $http.get('service/book.json').then(function(response) {
-    $scope.books = response.data;
-  });
-}
+  function BooksCtrl($scope, $http) {
+    $http.get('service/book.json').then(function(response) {
+      $scope.books = response.data;
+    });
+  }
 
-function BookMarksCtrl($scope, $http) {
-  $http.get('service/bookmark.json').then(function(response) {
-    $scope.bookmarks = response.data;
-  });
-}
+  function BookMarksCtrl($scope, $http) {
+    $http.get('service/bookmark.json').then(function(response) {
+      $scope.bookmarks = response.data;
+    });
+  }
 
-function NavCtrl($scope, $location) {
-  $scope.isActive = function(viewLocation) {
-    return viewLocation === $location.path();
-  };
-}
+  function NavCtrl($scope, $location) {
+    $scope.isActive = function(viewLocation) {
+      return viewLocation === $location.path();
+    };
+  }
+})();
